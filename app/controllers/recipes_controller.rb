5c2db75a75e607bad9eec614c9ae009fa39b6094
MrFuentastic <vfuentes8587@gmail.com>
2017-09-05 20:45:39 -0500

    added routes, deleted unnecesary view pages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,7 @@
 class RecipesController < ApplicationController
+ 
   def index
     @recipes = Recipe.all
-  end
-
-  def show
-    @recipe = Recipe.find(params[:id])
   end
 
   def new
@@ -19,5 +16,34 @@ class RecipesController < ApplicationController
                           directions: params[:directions]
                           )
     recipe.save
+    redirect_to "/recipes/#{recipe.id}"
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end  
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    recipe = Recipe.find(params[:id])
+
+    recipe.assign_attributes(
+                              title: params[:title],
+                              chef: params[:chef],
+                              ingredients: params[:ingredients],
+                              directions: params[:directions]
+                              )
+
+    recipe.save
+    redirect_to "/recipes/#{recipe.id}"    
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    redirect_to "/recipes"
   end
 end
