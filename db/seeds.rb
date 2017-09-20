5aaa36835dd843b8ac8f94582c89c2507c63211a
MrFuentastic <vfuentes8587@gmail.com>
2017-09-19 21:42:29 -0500

    added category model and join table

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-recipe = Recipe.new(
-                    title:"Hand Sandwich",
-                    chef:"Josh",
-                    ingredients:"left hand, right hand, lettuce, tomato, mayo, cheese",
-                    directions:"place ingredients on left hand, put right hand on top, enjoy"
-                    )
+# recipe = Recipe.new(
+#                     title:"Hand Sandwich",
+#                     chef:"Josh",
+#                     ingredients:"left hand, right hand, lettuce, tomato, mayo, cheese",
+#                     directions:"place ingredients on left hand, put right hand on top, enjoy"
+#                     )
 
-recipe.save
+# recipe.save
 
-recipe = Recipe.new(
-                    title:"Garbage Lagsana",
-                    chef:"Bad Josh",
-                    ingredients:"old chinese food, socks, banana peel, hot pepper",
-                    directions:"layer ingredients in a pan, preheat oven to 350, let cook for 5 hours, let cool"
-                    )
+# recipe = Recipe.new(
+#                     title:"Garbage Lagsana",
+#                     chef:"Bad Josh",
+#                     ingredients:"old chinese food, socks, banana peel, hot pepper",
+#                     directions:"layer ingredients in a pan, preheat oven to 350, let cook for 5 hours, let cool"
+#                     )
 
-recipe.save
+# recipe.save
+
+Category.create!(name: "Delicious")
+Category.create!(name: "Disgusting")
+Category.create!(name: "Easy")
+Category.create!(name: "Carnivorous")
+
+CategoryRecipe.create!(recipe_id: 1, category_id: 2)
+CategoryRecipe.create!(recipe_id: 1, ccategory_id: 4)
+CategoryRecipe.create!(recipe_id: 2, category_id: 3)
+CategoryRecipe.create!(recipe_id: 2, category_id: 2)
+CategoryRecipe.create!(recipe_id: 4, category_id: 1)
+CategoryRecipe.create!(recipe_id: 4, category_id: 3)
+CategoryRecipe.create!(recipe_id: 6, category_id: 1)
+CategoryRecipe.create!(recipe_id: 6, category_id: 3)
